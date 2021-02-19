5712f5a4caf9ccb477350527376a68bbb88dbb97
Scott Malone <scottmcarpentry@gmail.com>
2021-02-19 15:58:07 +1000

    check comparison

---
+++ backend_tests/test_food.py
@@ -0,0 +1,16 @@
+import unittest
+from main import db
+
+class TestShelters(unittest.TestCase):
+    #Runs before test
+
+    @classmethod
+    def setUp(cls):
+        pass
+
+    @classmethod    
+    def tearDown(cls):
+        pass
+    
+    
+    
