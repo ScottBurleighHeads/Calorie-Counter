1fd5177747c205665b1f46f91f93b1c52ce52113
Scott Malone <scottmcarpentry@gmail.com>
2021-03-01 18:15:59 +1000

    Adjustment to Calorie Check. Fully functioning

--- controllers/Auth.py
+++ controllers/Auth.py
@@ -1,5 +1,7 @@
 from flask import Blueprint,json, render_template,request
 import requests
-auth = Blueprint("auth",__name__)
+auth = Blueprint("auth",__name__,url_prefix="login")
 
-@auth.route("")
+@auth.route("/",method=["POST"])
+def login():
+    r
--- controllers/food.py
+++ controllers/food.py
@@ -4,8 +4,13 @@ import requests
 food = Blueprint("food",__name__,url_prefix="/food")
 
 
-@food.route("/", methods=["GET","POST"])
+
+@food.route("/")
 def food_home():
+    return render_template("Calorie_counter.html")
+
+@food.route("/search", methods=["POST"])
+def food_search():
     result = request.form["search"]
     response = requests.get(f"https://api.edamam.com/api/food-database/v2/parser?ingr={result}&app_id=d50c6ade&app_key=71f98be59c54d6d436f8e639b4190f43")
     dict_response=json.loads(response.text) 
@@ -13,17 +18,17 @@ def food_home():
     length = len(dict_response["hints"])
     return render_template("Calorie_counter.html", values=values, length=length)
 
-@food.route("/<string:food>", methods=["GET"])
-def search_food(food):
-    response = requests.get(f"https://api.edamam.com/api/food-database/v2/parser?ingr={food}&app_id=d50c6ade&app_key=71f98be59c54d6d436f8e639b4190f43")
-    dict_response=json.loads(response.text) 
-    values=dict_response["hints"]
-    length = len(dict_response["hints"])
-    return render_template("Calorie_counter.html", values=values, length=length)
+# @food.route("/<string:food>", methods=["GET"])
+# def search_food(food):
+#     response = requests.get(f"https://api.edamam.com/api/food-database/v2/parser?ingr={food}&app_id=d50c6ade&app_key=71f98be59c54d6d436f8e639b4190f43")
+#     dict_response=json.loads(response.text) 
+#     values=dict_response["hints"]
+#     length = len(dict_response["hints"])
+#     return render_template("Calorie_counter.html", values=values, length=length)
 
-@food.route("/test/<string:food>", methods=["GET"])
-def search_foods(food):
-    response = requests.get(f"https://api.edamam.com/api/food-database/v2/parser?ingr={food}&app_id=d50c6ade&app_key=71f98be59c54d6d436f8e639b4190f43")
-    dict_response=json.loads(response.text) 
-    return dict_response
+# @food.route("/test/<string:food>", methods=["GET"])
+# def search_foods(food):
+#     response = requests.get(f"https://api.edamam.com/api/food-database/v2/parser?ingr={food}&app_id=d50c6ade&app_key=71f98be59c54d6d436f8e639b4190f43")
+#     dict_response=json.loads(response.text) 
+#     return dict_response
 
