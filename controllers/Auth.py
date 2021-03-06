a416b0fb715361198d3415b8ddf7c00791c1fcdc
Scott Malone <scottmcarpentry@gmail.com>
2021-03-06 16:47:30 +1000

    Completing log in controller

--- controllers/Auth.py
+++ controllers/Auth.py
@@ -1,9 +1,24 @@
-from flask import Blueprint,json, render_template,request
-import requests
+from models.user import User
+from flask import Blueprint,json, render_template,request, jsonify, abort
+from schemas.User_schema import user_schema, users_schema 
+from main import db
+from main import bcrypt
+from datetime import timedelta
+
+
 auth = Blueprint("auth",__name__,url_prefix="/login")
 
 # There are login and log out test clients
-@auth.route("/",methods=["GET"])
+@auth.route("/",methods=["POST"])
 def login():
-    return "Hello"
-
+    user_fields = user_schema.load(request.json)
+    user = User.query.filter_by(email=user_fields["email"]).first()
+    print(user_fields["password"])
+    print(user.password)
+    if not user or not bcrypt.check_password_hash(user.password, user_fields["password"]):
+        return abort(401, description="Incorrect username and password")
+    
+    expiry = timedelta(days=1) # Expires after one day
+    access_token = create_access_token(identity=str(user.id), expires_delta=expiry)
+    return jsonify(user_schema.dump(user))
+    
--- controllers/recipe.py
+++ controllers/recipe.py
@@ -5,27 +5,23 @@ import requests
 import os
 recipe = Blueprint("recipe",__name__,url_prefix="/recipe")
 
-# @recipe.route("/")
-# def hello():
-   
-#     url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search"
+@recipe.route("/")
+def home_recipe():
+    return render_template("recipe.html")
 
-#     querystring = {"query":"chicken burger"}#,"diet":"vegetarian","excludeIngredients":"coconut","intolerances":"egg, gluten","number":"10","offset":"0","type":"main course"}
-
-#     headers = {
-#         'x-rapidapi-key': "2c61510f3bmshae4895a79feb61ap14b001jsn40a7389c2acd",
-#         'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-#         }
-
-#     response = requests.request("GET", url, headers=headers, params=querystring)
-#     return json.loads(response.text)
-
-@recipe.route("/", methods=["GET"])
+@recipe.route("/search", methods=["POST"])
 def recipe_search():
     
+    recipe = request.form["search"]
+    calorie_result = request.form["calorie"]
+    if not calorie_result:
+        calorie_result = 1000
     APP_ID = os.environ.get("APP_ID_RECIPE")
     APP_KEY = os.environ.get("APP_KEY_RECIPE")
-    response = requests.get(f"https://api.edamam.com/search?q=banana bread&app_id={APP_ID}&app_key={APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free")
+    string = f"https://api.edamam.com/search?q={recipe}&app_id={APP_ID}&app_key={APP_KEY}&from=0&to=3&calories={calorie_result}&health=alcohol-free"
+    print(string)
+    response = requests.get(f"https://api.edamam.com/search?q={recipe}&app_id={APP_ID}&app_key={APP_KEY}&from=0&to=10&calories={calorie_result}&health=alcohol-free")
+    
     dict_response=json.loads(response.text)
     length = len(dict_response["hits"])
 
